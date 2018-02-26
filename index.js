21960fc6e12f9da0fa4eabe1d38dff221fc535fb
Itamar <doriitamar@gmail.com>
2018-02-26 20:39:45 +0200

    Now file type is added to the file object

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const parse = require('diff-parse');
+const parsingUtils = require('./lib/ParsingUtils');
 const gitUtils = require('./lib/GitUtils');
+
+(async function iifee() {
+  const diff = await gitUtils.getDiff('/Users/itamardori/Projects/steemRSS', 'origin/master', 'origin/pr-branch');
+  const files = parsingUtils.parseDiff(diff);
+  console.log(files);
+}());
 
 module.exports = async function parser(cwd, oldBranch, newBranch) {
   const diff = await gitUtils.getDiff(cwd, oldBranch, newBranch);
-  return parse(diff);
+  return parsingUtils.parseDiff(diff);
 };
