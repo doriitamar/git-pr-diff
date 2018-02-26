8ef5bf0b32a3f20358be7d3acbbf89fbdc1803d1
Itamar <doriitamar@gmail.com>
2018-02-26 20:40:38 +0200

    Removed unessecary code

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const parsingUtils = require('./lib/ParsingUtils');
 const gitUtils = require('./lib/GitUtils');
 
-(async function iifee() {
-  const diff = await gitUtils.getDiff('/Users/itamardori/Projects/steemRSS', 'origin/master', 'origin/pr-branch');
-  const files = parsingUtils.parseDiff(diff);
-  console.log(files);
-}());
-
 module.exports = async function parser(cwd, oldBranch, newBranch) {
   const diff = await gitUtils.getDiff(cwd, oldBranch, newBranch);
   return parsingUtils.parseDiff(diff);
